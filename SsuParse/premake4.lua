bf7ff7328874253374a3a44efb27e40d126f1d2e
Soar Qin <soarchin@gmail.com>
2016-06-12 11:58:22 +0800

    fixed premake

--- SsuParse/premake4.lua
+++ SsuParse/premake4.lua
@@ -6,8 +6,9 @@ project "SsuParse"
 
     currPath = path.getdirectory(_SCRIPT)
 
+    configuration { "not vs*" }
+        buildoptions { "-std=c++11" }
     configuration { "windows", "not vs*" }
-        buildoptions { "-std=gnu++0x" }
         buildoptions { "-static-libgcc", "-static-libstdc++" }
         linkoptions { "-static-libgcc", "-static-libstdc++" }
     configuration { "windows" }
